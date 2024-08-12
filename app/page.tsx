2fbfe17259261590addf1b0fb2fed1660cbc19a5
Jon Taylor <heyjtaylor@gmail.com>
2024-08-12 16:18:33 +0300

    fixed window console warning

--- app/page.tsx
+++ app/page.tsx
@@ -7,25 +7,35 @@ import {
   defaultServices,
   defaultConfig,
   BOT_READY_TIMEOUT,
+  Voice,
 } from "@/rtvi.config";
 import { VoiceClientAudio, VoiceClientProvider } from "realtime-ai-react";
 import { DailyVoiceClient } from "realtime-ai-daily";
 import { TooltipProvider } from "@radix-ui/react-tooltip";
-import { useState } from "react";
-
-const voiceClient = new DailyVoiceClient({
-  baseUrl: process.env.NEXT_PUBLIC_BASE_URL || "/api",
-  services: defaultServices,
-  config: defaultConfig,
-  enableMic: true,
-  timeout: BOT_READY_TIMEOUT,
-});
+import { useEffect, useRef, useState } from "react";
 
 export default function Home() {
   const [showSplash, setShowSplash] = useState(true);
+  const voiceClientRef = useRef<DailyVoiceClient | null>(null);
+
+  useEffect(() => {
+    if (voiceClientRef.current) {
+      return;
+    }
+
+    const voiceClient = new DailyVoiceClient({
+      baseUrl: process.env.NEXT_PUBLIC_BASE_URL || "/api",
+      services: defaultServices,
+      config: defaultConfig,
+      enableMic: true,
+      timeout: BOT_READY_TIMEOUT,
+    });
+
+    voiceClientRef.current = voiceClient;
+  }, []);
 
   return (
-    <VoiceClientProvider voiceClient={voiceClient}>
+    <VoiceClientProvider voiceClient={voiceClientRef.current!}>
       <TooltipProvider>
         <main>
           {showSplash ? (
