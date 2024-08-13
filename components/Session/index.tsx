fb22fe945266fe3a9742dd9c05546b0b2021387b
Jon Taylor <heyjtaylor@gmail.com>
2024-08-13 19:47:46 +0300

    temporarily remove started state so mic is unmuted

--- components/Session/index.tsx
+++ components/Session/index.tsx
@@ -26,7 +26,8 @@ interface SessionProps {
 export const Session = React.memo(
   ({ state, onLeave, startAudioOff = false }: SessionProps) => {
     const voiceClient = useVoiceClient()!;
-    const [hasStarted, setHasStarted] = useState(false);
+    const [hasStarted, setHasStarted] = useState(true);
+    //const [hasStarted, setHasStarted] = useState(false);
     const [showDevices, setShowDevices] = useState(false);
     const [showStats, setShowStats] = useState(false);
     const [muted, setMuted] = useState(startAudioOff);
@@ -54,20 +55,20 @@ export const Session = React.memo(
       }, [])
     );
 
-    useVoiceClientEvent(
+    /*useVoiceClientEvent(
       VoiceEvent.BotStoppedSpeaking,
       useCallback(() => {
         if (hasStarted) return;
 
         setHasStarted(true);
       }, [hasStarted])
-    );
+    );*/
 
     // ---- Effects
 
     useEffect(() => {
       // Reset started state on mount
-      setHasStarted(false);
+      // setHasStarted(false);
     }, []);
 
     useEffect(() => {
