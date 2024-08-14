7a8f01a5ee7c691d1e5b8ef9fca2d54bee360c4d
Jon Taylor <heyjtaylor@gmail.com>
2024-08-15 00:14:15 +0300

    hide model badge

--- components/Session/Agent/model.tsx
+++ components/Session/Agent/model.tsx
@@ -1,19 +1,17 @@
 import React, { useCallback } from "react";
 import { VoiceEvent } from "realtime-ai";
-import { useVoiceClient, useVoiceClientEvent } from "realtime-ai-react";
+import { useVoiceClientEvent } from "realtime-ai-react";
 
 import styles from "./styles.module.css";
 
 const ModelBadge: React.FC = () => {
-  const { config } = useVoiceClient()!;
-  const [model, setModel] = React.useState<string | undefined>(
-    config?.llm?.model
-  );
+  //const { config } = useVoiceClient()!;
+  const [model, setModel] = React.useState<string | undefined>("Unknown model");
 
   useVoiceClientEvent(
     VoiceEvent.ConfigUpdated,
     useCallback((e) => {
-      setModel(e?.llm?.model);
+      setModel("Unknown model");
     }, [])
   );
 
