296ac9424eede94f294bc061462560f7dafc84c5
Jon Taylor <heyjtaylor@gmail.com>
2024-08-30 18:31:54 +0300

    added voices for languages

--- rtvi.config.ts
+++ rtvi.config.ts
@@ -30,28 +30,28 @@ export const LANGUAGES = [
     value: "de",
     tts_model: "sonic-multilingual",
     stt_model: "nova-2-general",
-    default_voice: "79a125e8-cd45-4c13-8a67-188112f4dd22",
+    default_voice: "b9de4a89-2257-424b-94c2-db18ba68c81a",
   },
   {
     label: "Portuguese",
     value: "pt",
     tts_model: "sonic-multilingual",
     stt_model: "nova-2-general",
-    default_voice: "79a125e8-cd45-4c13-8a67-188112f4dd22",
+    default_voice: "700d1ee3-a641-4018-ba6e-899dcadc9e2b",
   },
   {
     label: "Chinese",
     value: "zh",
     tts_model: "sonic-multilingual",
     stt_model: "nova-2-general",
-    default_voice: "79a125e8-cd45-4c13-8a67-188112f4dd22",
+    default_voice: "e90c6678-f0d3-4767-9883-5d0ecf5894a8",
   },
   {
     label: "Japanese",
     value: "ja",
     tts_model: "sonic-multilingual",
     stt_model: "nova-2-general",
-    default_voice: "79a125e8-cd45-4c13-8a67-188112f4dd22",
+    default_voice: "2b568345-1d48-4047-b25f-7baccf842eb0",
   },
 ];
 
